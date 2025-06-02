cdfd77f6185530ee7b8ca3eca074d07630d0fdf6
Justin Hold <36420615+jhold1982@users.noreply.github.com>
2025-06-02 11:35:29 -0500

    Initial Commit

---
+++ takeHomeTest/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  takeHomeTest
+//
+//  Created by Justin Hold on 6/2/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ takeHomeTest/takeHomeTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  takeHomeTestApp.swift
+//  takeHomeTest
+//
+//  Created by Justin Hold on 6/2/25.
+//
+
+import SwiftUI
+
+@main
+struct takeHomeTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
